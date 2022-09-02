6e22a526cbdcff125cc696f647043eb37bced81b
kellerlivia <rm94275@fiap.com.br>
2022-09-02 19:43:38 -0300

    exercicio 2

--- rm94275/cp1/src/App.jsx
+++ rm94275/cp1/src/App.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function App() {
+
+    const[aluno,setAluno] = useState(objAluno)
+
+    const objAluno = {
+        nome: "Joaquim",
+        rm: 99999,
+        turma: "1TDSZ"
+
+    }
+
+    const nota1 = 7
+    const nota2 = 8
+    const nota3 = 10
+
     return (
         <>
             <h1>CP1</h1>
