5f7048bac750748ab1893e3525e075f388c5dae8
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-09-02 20:16:48 -0300

    Solução do cálculo da média.

--- pf0670/cp1/src/components/mediaAluno/MediaAluno.jsx
+++ pf0670/cp1/src/components/mediaAluno/MediaAluno.jsx
@@ -1,9 +1,47 @@
 import React from "react"
 
 export default function MediaAluno(props) {
+
+    const objEstilo = {
+        color: "#000000",
+        fontSize: "2rem"
+    }
+
+    let media = 0
+
+    const calculaMedia = () => {
+        media = ((props.nota1 + props.nota2 + props.nota3) / 3)
+        if (media <= 0) {
+            objEstilo.color = "#FF0000"
+            return objEstilo
+        } else if (media > 0 && media < 6) {
+            objEstilo.color = "#FFA500"
+            return objEstilo
+        } else if (media >= 6) {
+            objEstilo.color = "#00FF00"
+            return objEstilo
+        }
+
+    }
+
     return (
         <div>
             <h2>COMPONENTE MÉDIA-ALUNO</h2>
+            <div>
+                <p><strong>Aluno : </strong> {props.aluno.nome}</p>
+                <p><strong>RM : </strong> {props.aluno.rm}</p>
+                <p><strong>Turma : </strong> {props.aluno.turma}</p>
+                <p><strong>MÉDIA : </strong> <span style={calculaMedia()}>{media}</span></p>
+            </div>
         </div>
     )
 }
+
+
+
+// 4 – No componente MediaAluno deve ser realizada através de uma função a média das
+// notas recebidas. De acordo com o resultado deve ser impresso o seguinte: ( 3 Pontos)
+// a) Se a média for igual a zero a nota deve ser apresentada com a cor vermelha.
+// b) Se a média for maior que zero e menor que 6 a nota deve ser apresentada com a cor
+// laranja.
+// c) Se a média for igual ou maior que 6 a nota deve ser apresentada com a cor verde.
