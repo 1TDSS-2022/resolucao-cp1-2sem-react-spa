19e81ae1a962fbba7b79b58b1774173e66edbcb7
WesleyKleim <wesleykleim@gmail.com>
2022-09-03 00:13:00 -0300

    Criando a média do aluno e incluindo cores.

--- rm94412/cp1/src/App.jsx
+++ rm94412/cp1/src/App.jsx
@@ -1,38 +1,30 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react"
+import { useState, useEffect } from "react"
 import MediaAluno from './components/MediaAluno/MediaAluno'
-export default function App(){
 
-   // const objAluno = {
-   //     nome: "Wesley",
-   //     rm: 94412,
-    //    turma: "1TDSS"
-   // }
+
+export default function App(){    
     
-    
-    const[aluno,setAluno] = useState({
+    const[aluno, setAluno] = useState({
         nome: "Wesley",
         rm: 94412,
-        turma:"1TDSS"
+        turma: "1TDSS"
     })
+   
 
-    const nota1 = 6
+    const nota1 = 5
     const nota2 = 8
-    const nota3 = 9
+    const nota3 = 10
 
 
     return(
         <>
             <h1>CP2 de RWD 2º Semestre</h1>
             <MediaAluno
-            nomeProps = {nome}
-            rmProps = {rm}
-            turmaProps = {turma}
-            nota1Props = {nota1}
-            nota2Props = {nota2}
-            nota3Props = {nota3}
-            
-
+                alunoProp = {aluno}
+                nota1Prop = {nota1}
+                nota2Prop = {nota2}
+                nota3Prop = {nota3}
             />
         </>
     )
