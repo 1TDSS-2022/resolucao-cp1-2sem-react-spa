8e3ebdb5c11a318f16e4da7f0ad37b6bd0998cf9
mariananieton <rm94141@fiap.com.br>
2022-09-28 16:15:51 -0300

    Questão 2 resolvida - useState + Objeto e consts

---
+++ rm94141/cp1/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+
+export default function App() {
+
+    const[aluno, setAluno] = useState({
+        nome: "Joaquim",
+        rm: 99999,
+        turma: "1TDSZ"
+    })
+
+    const nota1 = 5
+    const nota2 = 0
+    const nota3 = 2
+
+    return (
+        <>
+            <h1>CP1 de RWD 2º semestre</h1>
+        </>
+    )
+}
--- rm94141/cp1/src/index.js
+++ rm94141/cp1/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
 
 ReactDOM.render(
-    "Conteúdo",
+    <App/>,
 document.getElementById("root"))
