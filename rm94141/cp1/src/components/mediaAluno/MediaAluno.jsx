22b1a6045c57bac6b35d9403e1b219b41b49056b
mariananieton <rm94141@fiap.com.br>
2022-10-01 17:01:25 -0300

    Solução 2 da média do Aluno

--- rm94141/cp1/src/components/mediaAluno/MediaAluno.jsx
+++ rm94141/cp1/src/components/mediaAluno/MediaAluno.jsx
@@ -2,28 +2,22 @@ import React from "react"
 
 export default function MediaAluno(props) {
 
-    const objEstilo = {
-        color: "#000000",
-        fontSize: "2rem"
-    }
+const calculaMedia = () =>{
+    let media = ((props.nota1+props.nota2+props.nota3) / 3)
     
-    let media = 0
+    if(media <= 0){
 
-    const calculaMedia = () =>{
+        return <><strong>MÉDIA : </strong><span style={{color : "#FF0000"}}>{media}</span></>
 
-        media = ((props.nota1+props.nota2+props.nota3) / 3)
+    } else if(media > 0 && media < 6){
 
-        if(media <= 0){
-            objEstilo.color = "#FF0000"
-            return objEstilo
-        } else if(media > 0 && media < 6){
-            objEstilo.color = "#FFA500"
-            return objEstilo
-        } else if(media >= 6){
-            objEstilo.color = "#00FF00"
-            return objEstilo
-        }
+        return <><strong>MÉDIA : </strong><span style={{color : "#FFA500"}}>{media}</span></>
+
+    } else if(media >= 6){
+
+        return <><strong>MÉDIA : </strong><span style={{color : "#00FF00"}}>{media}</span></>
     }
+}
 
     return (
         <div>
@@ -32,8 +26,32 @@ export default function MediaAluno(props) {
                 <p><strong>Aluno : </strong> {props.aluno.nome}</p>
                 <p><strong>RM : </strong> {props.aluno.rm}</p>
                 <p><strong>Turma : </strong> {props.aluno.turma}</p>
-                <p><strong>MÉDIA : </strong><span style={calculaMedia()}>{media}</span></p>
+                <p>{calculaMedia()}</p>
             </div>
         </div>
     )
 }
+
+
+// let media = 0
+
+// const objEstilo = {
+//     color: "#000000",
+//     fontSize: "2rem"
+// }
+
+// const calculaMedia = () =>{
+
+//     media = ((props.nota1+props.nota2+props.nota3) / 3)
+
+//     if(media <= 0){
+//         objEstilo.color = "#FF0000"
+//         return objEstilo
+//     } else if(media > 0 && media < 6){
+//         objEstilo.color = "#FFA500"
+//         return objEstilo
+//     } else if(media >= 6){
+//         objEstilo.color = "#00FF00"
+//         return objEstilo
+//     }
+// }
